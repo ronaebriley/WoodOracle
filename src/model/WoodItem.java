f8a216357f129714edd1e0bbeb15908aee53b732
parrarodrigueze <esteban.parrarodriguez@belmont.edu>
2022-09-14 09:35:02 -0500

    Created WoodItem
    New class added to model the wood to be used in the system

---
+++ src/model/WoodItem.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * @author Esteban
+ * Class defining the elements of a wood type
+ */
+public class WoodItem {
+
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     * You may implement this method to fit your needs
+     */
+    @Override
+    public String toString() {
+        return "";
+    }
+
+}
