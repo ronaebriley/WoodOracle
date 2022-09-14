b8dfe3ce6a64a51519e9de46c70f6f8b87e7b741
parrarodrigueze <esteban.parrarodriguez@belmont.edu>
2022-09-14 09:42:06 -0500

    Created HardwoodSeller
    Removed Main.java and added HardwoodSeller as the entry point for the program.
    
    Added stubs for the methods required to read the input file and to compute the ETA

---
+++ src/controller/HardwoodSeller.java
@@ -0,0 +1,29 @@
+package controller;
+
+/**
+ * @author Esteban
+ *
+ */
+public class HardwoodSeller {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+    * Method for reading the input file to be processed by the Hardwood Seller
+     **/
+    public void readInputFile(String inputFilePath){
+
+    }
+
+    /**
+     * Method that computes the delivery ETA
+     **/
+    public Double deliveryTime(){
+        Double deliveryETA = 0.0;
+        return deliveryETA;
+    }
+
+}
