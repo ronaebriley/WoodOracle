00bee0c7a25efbfe33b40315e6263c302e1a73ad
parrarodrigueze <esteban.parrarodriguez@belmont.edu>
2022-09-14 09:37:23 -0500

    Updated WoodItem
    Modified access level for member data, added additional constructor and getter methods for the now private data

--- src/model/WoodItem.java
+++ src/model/WoodItem.java
@@ -6,11 +6,59 @@ package model;
  */
 public class WoodItem {
 
+    private String type;
+    private Double baseDeliveryTime;
+    private Double price;
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     * You may implement this method to fit your needs
+    /**
+     * Default constructor
      */
+    public WoodItem(){
+        this.type = "Empty";
+        this.baseDeliveryTime = 0.0;
+        this.price = 0.0;
+    }
+
+    /**
+     * Constructor of a WoodItem element with parameters
+     * @param type Name of the wood type
+     * @param daseDeliveryTime base delivery time
+     * @param price price per Bare Foot
+     */
+    public WoodItem(String type, Double daseDeliveryTime, Double price) {
+        this.type = type;
+        this.baseDeliveryTime = daseDeliveryTime;
+        this.price = price;
+    }
+
+    /**
+     * Get the type of Wood
+     *
+     * @return Name of the WoodItem
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Get the base delivery time
+     *
+     * @return the baseDeliverytime
+     */
+    public Double getDaseDeliveryTime() {
+        return baseDeliveryTime;
+    }
+
+    /**
+     *
+     * Get the price per Bare Foot
+     *
+     * @return the price for 1 Bare Foot of this WoodItem
+     */
+    public Double getPrice() {
+        return price;
+    }
+
     @Override
     public String toString() {
         return "";
